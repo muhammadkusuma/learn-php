54c3b508314ada2e759ddbadcc951ce1d04b6b9b
muhammadkusuma <mwiraadekusuma1@gmail.com>
2022-08-15 18:15:23 +0700

    switch statement

---
+++ switch-statement.php
@@ -0,0 +1,34 @@
+<?php
+
+$nilai="A";
+
+switch ($nilai){
+    case "A":
+        echo "Anda lulus dengan sangat baik" . PHP_EOL;
+        break;
+    case "B" :
+    case "C" :
+        echo "Anda Lulus" . PHP_EOL;
+        break;
+    case "D" :
+        echo "Anda tidak lulus" . PHP_EOL;
+        break;
+    default :
+        echo "Mungkin anda salah jurusan" . PHP_EOL;
+}
+
+//syntax alternatif
+switch ($nilai) :
+    case "A":
+        echo "Anda lulus dengan sangat baik" . PHP_EOL;
+        break;
+    case "B" :
+    case "C" :
+        echo "Anda Lulus" . PHP_EOL;
+        break;
+    case "D" :
+        echo "Anda tidak lulus" . PHP_EOL;
+        break;
+    default :
+        echo "Mungkin anda salah jurusan" . PHP_EOL;
+endswitch;
