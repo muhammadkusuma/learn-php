ef66c5a94bf707f7dd4ec4b969a2f519551ebca9
muhammadkusuma <mwiraadekusuma1@gmail.com>
2022-08-16 20:55:09 +0700

    goto operator

---
+++ goto-operator.php
@@ -0,0 +1,19 @@
+<?php
+
+goto a;
+echo "Hello Dunia" . PHP_EOL;
+
+a:
+echo "Hello A" . PHP_EOL;
+
+$counter=1;
+while (true){
+    echo "While loop $counter" . PHP_EOL;
+    $counter++;
+    if ($counter>10){
+        goto end;
+    }
+}
+
+end:
+echo "End Loop" . PHP_EOL;
