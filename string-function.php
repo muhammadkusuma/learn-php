ae17fac421ccedf6f59df1474bd284736a72924d
muhammadkusuma <mwiraadekusuma1@gmail.com>
2022-08-17 13:56:34 +0700

    string function

---
+++ string-function.php
@@ -0,0 +1,8 @@
+<?php
+
+var_dump(join(",",[1,2,3,4,5]));
+var_dump(explode(" ", "M. Wira Ade Kusuma"));
+var_dump(strtoupper("M. wira ade kusuma"));
+var_dump(strtolower("M. WIRA ADE KUSUMA"));
+var_dump(trim("          wira          "));
+var_dump(substr("Wira Ade Kusuma",0,3));
